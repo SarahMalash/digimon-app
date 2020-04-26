4c9969289b8c536a1c046ab2932b0a67f70b2c7f
SarahMalash <sarahabumalash@gmil.com>
2020-04-26 12:43:41 +0300

    exam

---
+++ data/schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS digimontable;
+CREATE TABLE digimontable(
+id SERIAL PRIMARY KEY,
+name VARCHAR (255),
+level VARCHAR(255),
+image VARCHAR(255)
+
+);
---
+++ server.js
@@ -0,0 +1,91 @@
+'use strict'
+//server
+const express= require ('express');
+const superagent = requuire ('superagent');
+const PORT = process.env.PORT||4000;
+const pg = require('pg');
+const app = express();
+const methodeOverride = require;
+
+// connect to psql
+const client=new pg.Client(process.env.DATABASE_URL);
+client.on('error', (err)=>console.log(error));
+
+
+//routs
+app.use(express.static ('./pages'));
+app.use(express.json);
+
+
+app.use ('*', notFoundHandler);
+app.put ('/update/:id', updateHandler);
+app.delete('/delete/:id',deleteHandler);
+app.get('/favourites/:id', favHandler);
+app.post('/add', addToFav)
+
+function favHandler (req,res){
+    const SQL = 'SELECT * FROM digimontable WHERE id= $1;';
+    const safeValues = [req.body.params];
+    client
+    res.render //apage should be here, 
+{rows[0]}
+
+}
+
+function addToFav (req,res){
+const {name,level,image}=req.body;
+const SQL = 'INSERT INTO digimontable (name, level,image;)';
+VALUES($1,$2,$3);
+client
+.query(SQL,safeValues).then ((result)=>res.redirect('/')
+)
+.catch (err=>errorHandler(err,req,res));
+}
+
+
+
+
+
+
+
+
+
+
+
+function updateHandler(req,res){
+const SQL = 'UPDATE digimontable SET name=$1, level=$2, image=$3, WHERE id =$4;';
+const safeValues = [req.body.name, req.body.level, req.body.image, req.params.id];
+client
+.query(SQL,safeValues).then ((result)=>res.redirect('/')
+)
+.catch (err=>errorHandler(err,req,res));
+}
+
+function deleteHandler(req,res){
+    const SQL ='DELETE FROM digimontable WHERE id=$1;';
+    const safeValues = [req.body.params];
+    client
+    .query(SQL,safeValues).then ((result)=>res.redirect('/')
+)
+.catch (err=>errorHandler(err,req,res));
+}
+
+
+
+
+// function Digimon (){
+//     this.name = name;
+//     this.level =level;
+//     this.image =image;
+
+// }
+
+
+
+
+function notFoundHandler (req,res){
+res.status(404).send ('sorry, page not foun');
+}
+ function errorHandler(req,res){
+rres.status(500).send ('sorry, error accured');
+ }
